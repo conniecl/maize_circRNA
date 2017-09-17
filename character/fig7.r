d5b8952ac2acb2e14e9ab2388f8d51ec1b9c6687
conniecl <lchen_boring@163.com>
2017-09-17 20:28:31 +0800

    Create fig7.r

---
+++ character/fig7.r
@@ -0,0 +1,21 @@
+##fig7c##
+library(ggplot2)
+data<-read.table("C:/Users/lchen/Desktop/fig7c.txt",header=T)
+p<-ggplot(data)+geom_boxplot(aes(x=rcp,y=log(circ,10),fill=rcp))+scale_fill_manual(values=c("red","blue"))+theme_bw()+theme(legend.position='none',panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.line = element_line(colour = "black"))
+ggsave(p,filename = "fig7c.svg",height = 4,width = 6)
+
+##fig7d##
+data<-read.table("C:/Users/lchen/Desktop/fig7d.txt",header=T)
+p<-ggplot(data,aes(x=circ,y=linear))+geom_point(fill="red",size=4,shape=21,color="darkgrey",alpha=3/5,stroke=1.5)+theme_bw()+theme(legend.position='none',panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.line = element_line(colour = "black"))
+ggsave(p,filename = "fig7d.svg",height = 4,width = 6)
+
+##fig7e##
+data<-read.table("C:/Users/lchen/Desktop/fig7e.txt",header=T)
+norcp<-data[which(data$rcp=="without"),]
+rcp<-data[which(data$rcp=="with"),]
+type<-c("with","without")
+mean<-c(mean(rcp$earheight),mean(norcp$earheight))
+sd<-c(sd(rcp$earheight),sd(norcp$earheight))
+data<-data.frame(type,mean,sd)
+p<-ggplot(data,aes(x=type,y=mean,fill=type))+geom_bar(stat="identity",width = .5)+geom_errorbar(aes(ymin=mean-sd,ymax=mean+sd),width=.2,position = "dodge")+scale_fill_manual(values=c("red","blue"))+theme_bw()+theme(legend.position='none',panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.line = element_line(colour = "black"))
+ggsave(p,filename = "fig7e.svg",height = 4,width = 6)
