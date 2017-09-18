e79887b8039fdc490acb50fd4d5f4e566b58b34b
conniecl <lchen_boring@163.com>
2017-09-18 16:55:09 +0800

    Create b73_gene_len.pl

---
+++ character/b73_gene_len.pl
@@ -0,0 +1,17 @@
+#!usr/bin/perl -w
+open GFF,"<Zea_mays.AGPv3.26.gff3" or die "cannot open the file $!";
+open OUT,">Zea_mays.AGPv3.26_genelen.gff3" or die "cannot open the file $!";
+readline GFF;
+while(<GFF>)
+{
+    chomp;
+    @tmp=split("\t",$_);
+    if($tmp[2] eq "gene")
+    {
+        $genename=(split(":|;",$tmp[-1]))[1];
+        $length=$tmp[4]-$tmp[3]+1;
+        print OUT "$genename\t$length\n";
+    }
+}
+close GFF;
+close OUT;
